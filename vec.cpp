bc8da147298f04076c3f0ad0474d0544b50ac8ab
zachary-hills <zachary.hills@uoit.net>
2020-01-24 20:13:43 -0500

    started vec3 class

--- vec.cpp
+++ vec.cpp
@@ -20,6 +20,10 @@ Vec2::Vec2(const double x, const double y) : x((float)x), y((float)y)
 {
 }
 
+Vec2::Vec2(const Vec3& v) :x(v.x), y(v.y)
+{
+}
+
 Vec2 Vec2::operator*(const Vec2& v)const
 {
 	return Vec2();
@@ -177,4 +181,206 @@ std::string Vec2::toString() const
 float Vec2::angle(const Vec2& v1, const Vec2& v2)
 {
 	return atan2f(v1.x * v2.y - v1.y*v2.x, v1.x*v2.x + v1.y*v2.y);
+}
+
+Vec3::Vec3():x(0.0f),y(0.0f),z(0.0f)
+{
+}
+
+Vec3::Vec3(const float x, const float y, const float z) : x(x), y(y), z(z)
+{
+}
+
+Vec3::Vec3(const int x, const int y, const int z) : x((float)x), y((float)y), z((float)z)
+{
+}
+
+Vec3::Vec3(const double x, const double y, const double z): x((float)x), y((float)y), z((float)z)
+{
+}
+
+Vec3::Vec3(const Vec2& v) : x(v.x), y(v.y), z(0.0f)
+{
+}
+
+Vec3::Vec3(const Vec2& v, const float z) : x(v.x), y(v.y), z(z)
+{
+}
+
+Vec3::Vec3(const Vec2& v, const int z) : x(v.x), y(v.y), z((float)z)
+{
+}
+
+Vec3::Vec3(const Vec2& v, const double z) : x(v.x), y(v.y), z((float)z)
+{
+}
+
+Vec3 Vec3::operator*(const Vec3& v) const
+{
+	return Vec3();
+}
+
+Vec3 Vec3::operator*(const float scalar) const
+{
+	return Vec3(x*scalar, y*scalar, z*scalar);
+}
+
+void Vec3::operator*=(const float scalar)
+{
+	x *= scalar;
+	y *= scalar;
+	z *= scalar;
+}
+
+Vec3 Vec3::operator*(const int scalar) const
+{
+	float tmp = (float)scalar;
+	return Vec3(tmp*x, tmp*y, tmp*z);
+}
+
+void Vec3::operator*=(const int scalar)
+{
+	float tmp = (float)scalar;
+	x *= tmp;
+	y *= tmp;
+	z *= tmp;
+}
+
+Vec3 Vec3::operator*(const double scalar) const
+{
+	float tmp = (float)scalar;
+	return Vec3(x*tmp, y*tmp, z*tmp);
+}
+
+void Vec3::operator*=(const double scalar)
+{
+	const float tmp = (float)scalar;
+	x *= tmp;
+	y *= tmp;
+	z *= tmp;
+}
+
+Vec3 Vec3::operator/(const float scalar) const
+{
+	return Vec3(x/scalar, y/scalar, z/scalar);
+}
+
+void Vec3::operator/=(const float scalar)
+{
+	x /=scalar;
+	y /=scalar;
+	z /=scalar;
+}
+
+Vec3 Vec3::operator/(const int scalar) const
+{
+	const float tmp = (float)scalar;
+	return Vec3(x/tmp,y/tmp,z/tmp);
+}
+
+void Vec3::operator/=(const int scalar)
+{
+	const float tmp = (float)scalar;
+	x /= tmp;
+	y /= tmp;
+	z /= tmp;
+}
+
+Vec3 Vec3::operator/(const double scalar) const
+{
+	float tmp = (float)scalar;
+	return Vec3(x/tmp,y/tmp,z/tmp);
+}
+
+void Vec3::operator/=(const double scalar)
+{
+	const float tmp = (float)scalar;
+	x /= tmp;
+	y /= tmp;
+	z /= tmp;
+}
+
+Vec3 Vec3::operator+(const Vec3& v) const
+{
+	return Vec3(x+v.x,y+v.y,z+v.z);
+}
+
+void Vec3::operator+=(const Vec3& v)
+{
+	x += v.x;
+	y += v.y;
+	z += v.z;
+}
+
+Vec3 Vec3::operator-() const
+{
+	return Vec3(-x,-y,-z);
+}
+
+Vec3 Vec3::operator+() const
+{
+	return Vec3(abs(x),abs(y),abs(z));
+}
+
+Vec3 Vec3::operator-(const Vec3& v) const
+{
+	return Vec3(x-v.x,y-v.y,z-v.z);
+}
+
+void Vec3::operator-=(const Vec3& v)
+{
+	x -= v.x;
+	y -= v.y;
+	z -= v.z;
+}
+
+bool Vec3::operator==(const Vec3& v) const
+{
+	return (x==v.x && y==v.y && z==v.z);
+}
+
+bool Vec3::operator!=(const Vec3& v) const
+{
+	return (x!=v.x || y!=v.y || z!=v.z);
+}
+
+float Vec3::operator[](const unsigned int index) const
+{
+	return xyz[index];
+}
+
+float Vec3::magnitude() const
+{
+	return sqrtf(x*x+y*y+z*z);
+}
+
+float Vec3::sqrMagnitude() const
+{
+	return x*x+y*y+z*z;
+}
+
+Vec3 Vec3::normalize() const
+{
+	const float mag = magnitude();
+	return Vec3(x/mag,y/mag,z/mag);
+}
+
+Vec3 Vec3::rotate(const float degrees) const
+{
+	return Vec3();
+}
+
+Vec3 Vec3::cross() const
+{
+	return Vec3();
+}
+
+std::string Vec3::toString() const
+{
+	return std::string("x: " + std::to_string(x) + " y: " + std::to_string(y) + " z: " + std::to_string(z));
+}
+
+float Vec3::angle(const Vec3& v1, const Vec3& v2)
+{
+	return 0.0f;
 }
--- vec.h
+++ vec.h
@@ -1,12 +1,82 @@
 #pragma once
 #include <string>
 
+class Vec2;
+
+class Vec3 {
+public:
+	Vec3();
+	Vec3(const float x, const float y, const float z);
+	Vec3(const int x, const int y, const int z);
+	Vec3(const double x, const double y, const double z);
+	Vec3(const Vec2& v);
+	Vec3(const Vec2& v, const float z);
+	Vec3(const Vec2& v, const int z);
+	Vec3(const Vec2& v, const double z);
+
+	Vec3 operator*(const Vec3& v)const;
+
+	Vec3 operator*(const float scalar)const;
+	void operator*=(const float scalar);
+
+	Vec3 operator*(const int scalar)const;
+	void operator*=(const int scalar);
+
+	Vec3 operator*(const double scalar)const;
+	void operator*=(const double scalar);
+
+	Vec3 operator/(const float scalar)const;
+	void operator/=(const float scalar);
+
+	Vec3 operator/(const int scalar)const;
+	void operator/=(const int scalar);
+
+	Vec3 operator/(const double scalar)const;
+	void operator/=(const double scalar);
+
+	Vec3 operator+(const Vec3& v)const;
+	void operator+=(const Vec3& v);
+
+	Vec3 operator-() const;
+
+	Vec3 operator+() const;
+
+	Vec3 operator-(const Vec3& v)const;
+	void operator-=(const Vec3& v);
+
+	bool operator==(const Vec3& v) const;
+	bool operator!=(const Vec3& v) const;
+
+	float operator[](const unsigned int index)const;
+
+	float magnitude()const;
+	float sqrMagnitude()const;
+	Vec3 normalize()const;
+
+	Vec3 rotate(const float degrees)const;
+
+	Vec3 cross() const;
+	std::string toString() const;
+
+	union {
+		float xyz[3];
+		struct {
+			float x, y,z;
+		};
+	};
+
+	static float angle(const Vec3& v1, const Vec3& v2);
+	static Vec3 UP;
+	static Vec3 LEFT;
+};
+
 class Vec2 {
 public:
 	Vec2();
 	Vec2(const float x, const float y);
 	Vec2(const int x, const int y);
 	Vec2(const double x, const double y);
+	Vec2(const Vec3& v);
 
 	Vec2 operator*(const Vec2& v)const;
 
@@ -62,6 +132,5 @@ public:
 	static float angle(const Vec2& v1, const Vec2& v2);
 	static Vec2 UP;
 	static Vec2 LEFT;
-
-
 };
+
